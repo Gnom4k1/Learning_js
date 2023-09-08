26f67815bd6dfd6489bd0f3e27ddde5c7119d079
Rayan_Osling <100954683+Gnom4k1@users.noreply.github.com>
2023-09-08 19:25:22 +0300

    js

---
+++ js.js
@@ -0,0 +1,128 @@
+
+
+// Однострочный комментарий
+/* Многострочный комментарий */
+
+ //str, my_str, myStr, a1, $, $a допустимые имена переменных
+
+// Переменные
+// Ключевое слово: var, let, const (var идет нафиг, дед какой-то)
+// Формат LowerCamelCase (Верблюжья нотация)
+
+// var leftBoardWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(pi);
+
+// Типы данных, классификация
+
+// Примитивный тип данных
+
+//Строка - string
+// Двойные кавычки, одинарные кавычки (простые), обратные кавычки (имеют расширенный функционал)
+
+// let string = "Hello";
+// console.log(string);
+
+// let name = 'ыVan';
+// console.log(name);
+
+
+// let name1 = 'ЫVan';
+// alert( `Привет, пупсик по имени ${name1}!` );
+// console.log(name1);
+
+// Числа (number)
+
+// let number = 993
+// console.log(number)
+
+//Специальный тип данных
+
+//null Специальное значение, которое представляет отсутствующий объект 
+
+// console.log(something);
+
+//undefined Предаставляет значение переменой которая не была инициализирована
+
+// let numb;
+// console.log(numb)
+
+
+//NaN Специальное значение "не число", которое говорит о том, что была выполнена бессмысленная операция
+
+// console.log("Brawl Stars" * 993);
+
+//Infinity Бесконечность, известная ошибка при делении на 0
+
+// console.log(993 / 0);
+
+// Логический тип данных boolean (true, false)
+
+// let boolean = true;
+// console.log(boolean);
+
+// // Абстрактный тип данных object{}, это набор свойств и значений
+
+// let obj = {};
+
+// let persone = {
+//     name: "Ыvan", 
+//     age: 993,
+//     isMarriend: true
+// };
+// console.log(persone["age"]);
+
+//Массив
+
+// let arr = [993, 986, 979];
+// console.log(arr[0]);
+
+//Простое общение с пользователем
+
+// alert("Hello ЫVan")
+
+// let answer = confirm("Are you ЫVan?")
+// console.log(answer)
+
+//typeoff для определения типа данных
+// let answer = prompt("Отправить вашего друга в Огайо?", "Да");
+// console.log(typeof(answer));
+
+// Опраторы js
+//Унарный +
+
+// console.log("arr" + "-object");
+// console.log(993 + "-7");
+
+// Инкримент и декримент
+// ++префиксный инкремент
+// --постфиксный декримент
+
+// let incr = 10;
+//     decr = 10;
+
+// console.log(incr++);
+// console.log(--decr);
+
+// Остаток от деления
+
+// console.log(993%7);
+
+//= присваивание
+//== проверка на равенство
+
+// console.log("993" == 993);
+
+// Оператор && (И)
+
+// let isCheked = true;
+//     isCloset = true;
+// console.log(isCheked && isCloset);
+
+// Оператор ||(ИЛИ)
+
+// let isCheked = true;
+//     isCloset = false;
+// console.log(isCheked || isCloset);
